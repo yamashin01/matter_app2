6a24b69d1cabb9b5e3b6f3fb134389726abd487a
yamada <shinya.yamada@benext-corp.com>
2022-07-03 15:17:03 +0900

    ✨ add auth system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import { Auth } from "@supabase/ui";
+import { client } from "src/libs/supabase";
+import { AuthLayout } from "src/layout/AuthLayout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div>
+      <Auth.UserContextProvider supabaseClient={client}>
+        <AuthLayout>
+          <Component {...pageProps} />
+        </AuthLayout>
+      </Auth.UserContextProvider>
+    </div>
+  );
+};
 
 export default MyApp;
