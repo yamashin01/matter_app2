2e61e2c62eacc55f5a55c497aaccf7c0ae291369
yamada <shinya.yamada@benext-corp.com>
2022-07-05 09:49:59 +0900

    ⬆️ add headlessui/react and mantine/dates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // media or class or false
   theme: {
