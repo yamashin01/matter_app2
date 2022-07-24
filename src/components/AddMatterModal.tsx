ccecdbdd0a9f2990433acc136dc8dc45b2fba1ad
yamada <shinya.yamada@benext-corp.com>
2022-07-25 08:38:31 +0900

    ⚡️ create costItemList in typelist.tsx

--- src/components/AddMatterModal.tsx
+++ src/components/AddMatterModal.tsx
@@ -21,6 +21,7 @@ import { addMatterData } from "src/libs/supabase";
 import {
   basicObjType,
   classificationList,
+  costItemList,
   costObjType,
   teamList,
 } from "src/utils/typelist";
@@ -104,19 +105,7 @@ export const AddMatterModal = (props: Props) => {
         placeholder="品目を選択してください。"
         className="col-span-3"
         required
-        data={[
-          { value: "備品購入", label: "備品購入" },
-          { value: "システム料", label: "システム料" },
-          { value: "施設利用料", label: "施設利用料" },
-          { value: "メンバー報酬", label: "メンバー報酬" },
-          { value: "外注費", label: "外注費" },
-          { value: "広告宣伝費", label: "広告宣伝費" },
-          { value: "シンラボ活動費", label: "シンラボ活動費" },
-          { value: "広告宣伝費", label: "広告宣伝費" },
-          { value: "教育・研修費", label: "教育・研修費" },
-          { value: "営業費", label: "営業費" },
-          { value: "その他", label: "その他" },
-        ]}
+        data={costItemList}
         {...costForm.getListInputProps("costList", index, "item")}
       />
       <Group className="col-span-2">
--- src/utils/typelist.tsx
+++ src/utils/typelist.tsx
@@ -1,12 +1,18 @@
 export const teamList = ["本部", "事業創出", "広報", "SDGs", "事務局"];
 
 export const classificationList = [
-  "会員費",
-  "受託案件",
-  "認定ファシリ",
-  "ボードゲーム",
-  "イベント",
-  "その他",
+export const costItemList = [
+  { value: "備品購入", label: "備品購入" },
+  { value: "システム料", label: "システム料" },
+  { value: "施設利用料", label: "施設利用料" },
+  { value: "メンバー報酬", label: "メンバー報酬" },
+  { value: "外注費", label: "外注費" },
+  { value: "広告宣伝費", label: "広告宣伝費" },
+  { value: "シンラボ活動費", label: "シンラボ活動費" },
+  { value: "広告宣伝費", label: "広告宣伝費" },
+  { value: "教育・研修費", label: "教育・研修費" },
+  { value: "営業費", label: "営業費" },
+  { value: "その他", label: "その他" },
 ];
 
 export interface basicObjType {
